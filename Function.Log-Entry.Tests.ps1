8fd87f7472c1c07f8c212c19612296669a5cd1a3
darrdog <ddarr@terrapower.com>
2023-12-21 12:40:00 -0800

    Pester Testing added

---
+++ Function.Log-Entry.Tests.ps1
@@ -0,0 +1,15 @@
+
+Describe 'Work Log-Entry' {
+    It 'Given a new entry PesterTester in work directory' {
+        Log-Entry -EntryName PesterTester -EntryPath work
+        Test-Path -Path "$env:USERPROFILE\Documents\Work_Log\$(get-date -f yyyy-MM-dd) PesterTester" | Should -Be True
+    }
+}
+
+Describe 'Training Log-Entry' {
+    It 'Given a new entry for PesterTester to the Train directory' {
+        Log-Entry -EntryName PesterTester -EntryPath Train
+        Test-Path -Path "$env:USERPROFILE\Documents\Training_log\$(get-date -f yyyy-MM-dd) PesterTester" | Should -Be True
+    }
+}
+
